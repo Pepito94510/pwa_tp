aa9ac579fd0b90a1c88880ae782624c888f8ddd8
Pepito94510 <kevin.boissiere94@gmail.com>
2023-04-20 16:48:20 +0200

    Add service_worker

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+
+import Home from './pages/Home';
+import Blog1 from './pages/Blog1';
+import Blog2 from './pages/Blog2';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Blog1" element={<Blog1 />}></Route>
+            <Route path="/Blog2" element={<Blog2 />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,20 @@
+function Home() {
+    return (
+      <>
+        <div className='Home'>
+          <h1>Home</h1>
+        </div>
+        <div>
+          <a href="/Blog1">
+            <button>Blog10</button>
+          </a>
+          <a href="/Blog2">
+            <button>Blog20</button>
+          </a>
+        </div>
+      </>
+  
+    );
+  }
+  
+  export default Home;
